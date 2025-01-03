7a1c458600688758096eaf905879e162dd906b80
harishhari130615 <125627378+harishsampleproj@users.noreply.github.com>
2025-01-03 20:53:31 +0530

    added compose files and modified frontend environment

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,13 @@
+  // export const environment = {
+  //   production: false,
+  //   baseUrl: 'http://localhost:3000/api',
+  // };
+  const isLocalhost = typeof window !== 'undefined' && window.location.origin.includes("localhost");
+
   export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:3000/api',
+    production: !isLocalhost,
+    baseUrl: isLocalhost
+      ? 'http://localhost:3000/api'
+      : 'https://eventdesk.io/api',
   };
   
