b4254948adda5ffbbf9b77742126f6cb67e0f17e
harishhari130615 <125627378+harishsampleproj@users.noreply.github.com>
2025-01-12 19:38:10 +0530

    testing rollback

--- backend/index.js
+++ backend/index.js
@@ -176,6 +176,6 @@ app.put('/update-admin', async (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(POR, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
