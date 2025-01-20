4b845619ccda7ce4b954d74c08c05ecd8bf4db98
harishhari130615 <125627378+harishsampleproj@users.noreply.github.com>
2025-01-20 23:18:27 +0530

    Updated loki host url

--- backend/logging.js
+++ backend/logging.js
@@ -3,7 +3,8 @@ const {createLogger , transports} = require('winston')
 const LokiTransport = require("winston-loki")
 
 const options = {
-    transports: [new LokiTransport({labels:{appName:'EventDesk'},host: 'http://127.0.0.1:3100'})],
+    transports: [new LokiTransport({labels:{appName:'EventDesk'},
+        host: 'http://78.47.29.73:3100'})],
   }
   
 const logger = createLogger(options)
